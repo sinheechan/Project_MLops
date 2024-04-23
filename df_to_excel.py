76dad2fc311aec44cca44532e7558735260838a1
sinheechan <gmlcks0905@gmail.com>
2024-04-23 14:41:25 +0900

    new file

---
+++ df_to_excel.py
@@ -0,0 +1,14 @@
+from pandas_datareader import data as pdr
+from datetime import datetime
+import pandas as pd
+import yfinance as yf
+yf.pdr_override()
+
+start = datetime(2017,1,1)
+end = datetime(2023,12,31)
+filename = datetime.now().strftime("%Y%m%d_%H%m%S")
+df = pdr.get_data_yahoo('005930.KS', start, end)
+
+print(df)
+
+df.to_excel(filename + '_df.xlsx')
