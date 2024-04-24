170dff54268fcfc318438f3201c25ee26761d7ab
sinheechan <gmlcks0905@gmail.com>
2024-04-24 18:58:29 +0900

    test

--- api_test.py
+++ api_test.py
@@ -8,6 +8,9 @@ param = {'s': '2017-01-01', 'e': '2022-12-31'} # parameter로 시작날짜, 종�
 res = requests.get(url, params=param)
 recv_data = res.text
 recv_data_to_json = json.loads(recv_data) # str -> json
+
 print(type(recv_data_to_json))
+
 df = pd.read_json(recv_data_to_json) # json -> dataframe
+
 print(df)
