db36a98013a9eee698398f138609240bd252c2c8
sinheechan <gmlcks0905@gmail.com>
2024-11-05 16:16:33 +0900

    test

--- api_test_db.py
+++ api_test_db.py
@@ -7,14 +7,14 @@ import json
 
 def DBtest():
     url = 'http://127.0.0.1:9999/getdatafromdb'
-    param = {'s': '2023-01-01', 'e': '2024-12-31'} # parameter로 시작날짜, 종료날짜 전달
+    param = {'s': '2023-01-01', 'e': '2024-12-31'}
     res = requests.get(url, params=param)
     print(res)
     recv_data_from_db = res.content
 
 if __name__ == '__main__':
     url = 'http://127.0.0.1:9999/getdatafromdb'
-    param = {'s': '2023-01-01', 'e': '2024-12-31'} # parameter로 시작날짜, 종료날짜 전달
+    param = {'s': '2023-01-01', 'e': '2024-12-31'}
     res = requests.get(url, params=param)
     recv_data = res.text
     print(recv_data, type(recv_data))
