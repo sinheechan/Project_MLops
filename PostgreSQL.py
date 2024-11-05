c087a6194de24b58f9df622c0f874120e8061cf4
sinheechan <gmlcks0905@gmail.com>
2024-11-05 14:01:28 +0900

    test

--- PostgreSQL.py
+++ PostgreSQL.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 # DBeaver에서 불러온 주식 데이터를 DataFrame으로 변환
-df = pd.DataFrame(주식_데이터)
+df = pd.DataFrame("주식_데이터")
 
 # PostgreSQL 연결 설정
 engine = create_engine("postgresql://username:password@host:port/dbname")
